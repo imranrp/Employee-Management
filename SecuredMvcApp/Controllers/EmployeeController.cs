cbd4a44c1cefd3fbe3b7ba6f6b0216ccb84ae7ef
Admin <Admin@DESKTOP-KSM5TIA>
2023-12-04 12:29:58 +0530

    Add project files.

---
+++ SecuredMvcApp/Controllers/EmployeeController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using EmployeeLibrary.Models;
+using Microsoft.AspNetCore.Authorization;
+namespace SecuredMvcApp.Controllers
+{
+    [Authorize]
+    public class EmployeeController : Controller
+    {
+        static HttpClient client = new HttpClient() { BaseAddress = new Uri("http://localhost:5256/api/Employee/") };
+
+        public async Task<ActionResult> Index()
+        {
+            string userName = User.Identity.Name;
+            string role = User.Claims.ToArray()[4].Value;
+            string secretKey = "My name is Bond, James Bond the great";
+            string token = await client.GetStringAsync("http://localhost:5256/api/Auth/?userName=" + userName + "&role=" + role + "&secretKey=" + secretKey);
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            List<Employee> employees = await client.GetFromJsonAsync<List<Employee>>("");
+            return View(employees);
+        }
+        public async Task<ActionResult> Details(int id)
+        {
+            Employee employee = await client.GetFromJsonAsync<Employee>("" + id);
+            return View(employee);
+        }
+
+        // GET: EmployeeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeController/Delete/5
+        [Authorize(Roles = "Admin,Dev")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            Employee employee = await client.GetFromJsonAsync<Employee>("" + id);
+            return View(employee);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                await client.DeleteAsync("" + id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SecuredMvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SecuredMvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
